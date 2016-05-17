d5ec1603cba9287c56b2d1ff27b2ea94b5eb829b
aenser <aaronenser1@gmail.com>
2016-05-17 14:01:06 -0400

    attraction form requires country selection then displays cities after

--- app/assets/javascripts/attractions/AttractionsCtrl.js
+++ app/assets/javascripts/attractions/AttractionsCtrl.js
@@ -1,4 +1,4 @@
-function AttractionsCtrl($location, Attraction, Country, City){
+function AttractionsCtrl($location, Attraction, Country, City, $scope){
   var ctrl = this;
 
   ctrl.countries = Country.query();
@@ -6,7 +6,17 @@ function AttractionsCtrl($location, Attraction, Country, City){
 
   ctrl.attraction = new Attraction();
   ctrl.attractions = Attraction.query();
+///////////////
+    // $scope.options1 = ctrl.countries;
+    // $scope.options2 = [];
+    // $scope.getOptions2 = function(){
+    //   var key = $scope.options1.indexOf($scope.option1);
+    //
+    //   var myNewOptions = ctrl.countries[key];
+    //   $scope.options2 = myNewOptions;
+    // }
 
+////////////////////////
   ctrl.addAttraction = function() {
     ctrl.attraction.$save(function() {
       $location.path('/home/attractions');
