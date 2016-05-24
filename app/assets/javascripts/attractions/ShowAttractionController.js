30733b02a9a88e659938b4fbdd8539a51d6707e5
benser1 <briganenser28@gmail.com>
2016-05-24 12:34:02 -0400

    maps working. working on filter

--- app/assets/javascripts/attractions/ShowAttractionController.js
+++ app/assets/javascripts/attractions/ShowAttractionController.js
@@ -1,26 +1,10 @@
-function ShowAttractionController ($stateParams, $scope, $rootScope, $http, $state, $location, uiGmapGoogleMapApi, Auth, City, Attraction, Country) {
-
+function ShowAttractionController ($stateParams, $scope, $http, $state, $location, uiGmapGoogleMapApi, Auth, City, Attraction, Country) {
 
   var ctrl = this;
 
   ctrl.attraction = Attraction.get({ id: $stateParams.id });
   ctrl.cities = City.query();
   ctrl.countries = Country.query();
-
-
-  /////////////
-  // ctrl.trips = Trip.query();
-  //
-  //
-  // ctrl.addToTrip = function() {
-  //   ctrl.trip = Trip.get({ id: $scope.trippy });
-  //   ctrl.attraction.trips.push(ctrl.trip);
-  //   ctrl.attraction.$update(function(result) {
-  //     console.log(result);
-  //   });
-  // };
-  /////////////
-
 
   Auth.currentUser()
   .then(function(user){
@@ -40,69 +24,6 @@ function ShowAttractionController ($stateParams, $scope, $rootScope, $http, $sta
       $location.path('/home/attractions');
     });
   };
-
-  ctrl.stateIsLoading = true;
-
-  ctrl.defaultLat = 44.2126995,
-  ctrl.defaultLong = -100.2471641,
-  ctrl.zoomA = 3;
-
-  ctrl.visitedList = [];
-
-  navigator.geolocation.getCurrentPosition(function(pos) {
-    ctrl.userLat = pos.coords.latitude;
-    ctrl.userLong = pos.coords.longitude;
-    ctrl.stateIsLoading = false;
-
-    uiGmapGoogleMapApi.then(function(maps) {
-
-      ctrl.mapA = { center: { latitude: ctrl.userLat || ctrl.defaultLat, longitude: ctrl.userLong || ctrl.defaultLong }, zoom: ctrl.zoomA };
-
-      $http.get('/attractions.json')
-      .success(function(data, response){
-        data.forEach(function(attraction){
-          var address = attraction.address + " " + attraction.city.name + " " + attraction.country.name + " " + attraction.zip
-          var geocoder = new google.maps.Geocoder();
-          geocoder.geocode( { "address": address }, function(results, status) {
-            if (status == google.maps.GeocoderStatus.OK && results.length > 0) {
-              var mapData = {};
-              mapData["id"] = attraction.id
-              mapData["options"] = {labelContent: attraction.name}
-              mapData["coords"] = {}
-              mapData["coords"]["latitude"] = results[0].geometry.location.lat()
-              mapData["coords"]["longitude"] = results[0].geometry.location.lng()
-              ctrl.visitedList.push(mapData);
-            }
-          });
-        })
-      })
-    })
-  })
-
-
-  // ctrl.addVisit = function(attraction){
-  //   ctrl.user.visits.push(attraction);
-  //   ctrl.user.$update(function(result){
-  //     console.log(result);
-  //   });
-  // };
-
-  // ctrl.logVisited = function(){
-  //   $http.post('/attractions/' + ctrl.attraction.id + '/visits').then(function(){
-  //     $state.go($state.current, {}, {reload: true});
-  //   })
-  // }
-  //
-  // ctrl.checkVisited = function(visitors){
-  //   var check;
-  //   for (var v in visitors) {
-  //     if (v.id == Auth._currentUser.id){
-  //       check = true;
-  //     }
-  //   }
-  //   return check;
-  // }
-
 }
 
 angular
--- app/assets/javascripts/maps/MapsCtrl.js
+++ app/assets/javascripts/maps/MapsCtrl.js
@@ -1,31 +1,26 @@
 function MapsCtrl($state, $stateParams, $http, $rootScope, uiGmapGoogleMapApi){
-  // var ctrl = this;
-  //
-  // // Define variables for our Map object
-  // ctrl.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
-
   var ctrl = this;
   ctrl.stateIsLoading = true;
 
   ctrl.defaultLat = 44.2126995,
   ctrl.defaultLong = -100.2471641,
-  ctrl.zoomA = 6;
+  ctrl.zoomA = 3;
 
   ctrl.visitedList = [];
-
-  ctrl.mapA = { center: { latitude: ctrl.userLat || ctrl.defaultLat, longitude: ctrl.userLong || ctrl.defaultLong }, zoom: ctrl.zoomA };
 
   navigator.geolocation.getCurrentPosition(function(pos) {
     ctrl.userLat = pos.coords.latitude;
     ctrl.userLong = pos.coords.longitude;
     ctrl.stateIsLoading = false;
 
-
     uiGmapGoogleMapApi.then(function(maps) {
+
+      ctrl.mapA = { center: { latitude: ctrl.userLat || ctrl.defaultLat, longitude: ctrl.userLong || ctrl.defaultLong }, zoom: ctrl.zoomA };
+
       $http.get('/attractions.json')
       .success(function(data, response){
         data.forEach(function(attraction){
-          var address = attraction.address + " " + attraction.city + " " + attraction.country + " " + attraction.zip
+          var address = attraction.address + " " + attraction.city.name + " " + attraction.country.name + " " + attraction.zip
           var geocoder = new google.maps.Geocoder();
           geocoder.geocode( { "address": address }, function(results, status) {
             if (status == google.maps.GeocoderStatus.OK && results.length > 0) {
@@ -41,7 +36,7 @@ function MapsCtrl($state, $stateParams, $http, $rootScope, uiGmapGoogleMapApi){
         })
       })
     })
-  });
+  })
 }
 
 angular
