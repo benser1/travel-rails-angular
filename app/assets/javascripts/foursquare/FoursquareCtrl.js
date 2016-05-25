e18a3456721012db68d68a7d0ecfaab762cc541a
aenser <aaronenser1@gmail.com>
2016-05-25 14:51:00 -0400

    working on country_id

--- app/assets/javascripts/foursquare/FoursquareCtrl.js
+++ app/assets/javascripts/foursquare/FoursquareCtrl.js
@@ -6,12 +6,14 @@ function FoursquareCtrl($scope, $http) {
 
   $scope.name = '';
 
+
   function fetch() {
-    $http.get("https://api.foursquare.com/v2/venues/search?client_id=11XT4KZISEOIM1TOXNMU5K4UE1YHRDLRGFIRV2LQBSD23DDU&client_secret=X30JWSQIESMYBIRITQMLBALXRCFA2Q0SVGMZWKMMVJMMZAIA&v=20160201&near=Atlanta&query=coffee_shop")
+    $http.get("https://api.foursquare.com/v2/venues/search?client_id=11XT4KZISEOIM1TOXNMU5K4UE1YHRDLRGFIRV2LQBSD23DDU&client_secret=X30JWSQIESMYBIRITQMLBALXRCFA2Q0SVGMZWKMMVJMMZAIA&v=20160201&near=Berlin&query=coffee_shop")
     .then(function(response) {
       $scope.venues = response.data;
     });
   };
+
 }
 
 
