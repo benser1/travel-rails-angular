851fb292fb764a9d2c8b6dbca9834e9726cfde90
benser1 <briganenser28@gmail.com>
2016-05-25 10:25:31 -0400

    foursquare basic search working

--- app/assets/javascripts/foursquare/FoursquareCtrl.js
+++ app/assets/javascripts/foursquare/FoursquareCtrl.js
@@ -1,20 +1,135 @@
-function FoursquareCtrl($scope, $http) {
+function FoursquareCtrl($scope, placesExplorerService, $filter) {
+  $scope.exploreNearby = "New York";
+  $scope.exploreQuery = "";
+  $scope.filterValue = "";
 
-  $scope.$watch('name', function() {
-    fetch();
-  });
+  $scope.places = [];
+  $scope.filteredPlaces = [];
+  $scope.filteredPlacesCount = 0;
 
-  $scope.name = '';
+  //paging
+  $scope.totalRecordsCount = 0;
+  $scope.pageSize = 10;
+  $scope.currentPage = 1;
 
-  function fetch() {
-    $http.get("https://api.foursquare.com/v2/venues/search?client_id=11XT4KZISEOIM1TOXNMU5K4UE1YHRDLRGFIRV2LQBSD23DDU&client_secret=X30JWSQIESMYBIRITQMLBALXRCFA2Q0SVGMZWKMMVJMMZAIA&v=20160201&near=Atlanta&query=coffee_shop")
-    .then(function(response) {
-      $scope.venues = response.data;
+  init();
+
+  function init() {
+
+    createWatche();
+    getPlaces();
+  }
+
+  function getPlaces() {
+
+    var offset = ($scope.pageSize) * ($scope.currentPage - 1);
+
+    placesExplorerService.get({ near: $scope.exploreNearby, query: $scope.exploreQuery, limit: $scope.pageSize, offset: offset }, function (placesResult) {
+
+      if (placesResult.response.groups) {
+        $scope.places = placesResult.response.groups[0].items;
+        $scope.totalRecordsCount = placesResult.response.totalResults;
+        filterPlaces('');
+      }
+      else {
+        $scope.places = [];
+        $scope.totalRecordsCount = 0;
+      }
+
     });
   };
-}
+
+  function filterPlaces(filterInput) {
+    $scope.filteredPlaces = $filter("placeNameCategoryFilter")($scope.places, filterInput);
+    $scope.filteredPlacesCount = $scope.filteredPlaces.length;
+  }
+
+  function createWatche() {
+
+    $scope.$watch("filterValue", function (filterInput) {
+      filterPlaces(filterInput);
+    });
+  }
+
+  $scope.doSearch = function () {
+
+    $scope.currentPage = 1;
+    getPlaces();
+
+  };
+
+  $scope.pageChanged = function (page) {
+
+    $scope.currentPage = page;
+    getPlaces();
+
+  };
+
+  // $scope.showVenuePhotos = function (venueId, venueName) {
+  //
+  //   placesPhotosService.get({ venueId: venueId }, function (photosResult) {
+  //
+  //     var modalInstance = $modal.open({
+  //       templateUrl: 'app/views/placesphotos.html',
+  //       controller: 'placesPhotosController',
+  //       resolve: {
+  //         venueName: function () {
+  //           return venueName;
+  //         },
+  //         venuePhotos: function () {
+  //           return photosResult.response.photos.items;
+  //         }
+  //       }
+  //     });
+  //
+  //     modalInstance.result.then(function () {
+  //       //$scope.selected = selectedItem;
+  //     }, function () {
+  //       //alert('Modal dismissed at: ' + new Date());
+  //     });
+  //
+  //   });
+  //
+  // };
+
+  // $scope.buildCategoryIcon = function (icon) {
+  //
+  //   return icon.prefix + '44' + icon.suffix;
+  // };
+  //
+  // $scope.buildVenueThumbnail = function (photo) {
+  //
+  //   return photo.items[0].prefix + '128x128' + photo.items[0].suffix;
+  // };
+
+  // $scope.bookmarkPlace = function (venue) {
+  //
+  //   if (!placesDataService.getUserInContext()) {
+  //
+  //     var modalInstance = $modal.open({
+  //       templateUrl: 'app/views/userprofile.html',
+  //       controller: 'userContextController',
+  //       resolve: {
+  //         venue: function () {
+  //           return venue;
+  //         }
+  //       }
+  //     });
+  //   }
+  //   else {
+  //     placesDataService.savePlace(venue).then(
+  //       function (results) {
+  //         // Do nothing as toaster showing from service
+  //       },
+  //       function (results) {
+  //         // Do nothing as toaster showing from service
+  //       });
+  //     }
+  //
+  //   };
+  }
 
 
-angular
-.module('app')
-.controller('FoursquareCtrl', FoursquareCtrl);
+  angular
+  .module('app')
+  .controller('FoursquareCtrl', FoursquareCtrl);
